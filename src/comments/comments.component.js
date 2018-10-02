cf6879423d533afacef50f418ec577d5b2f040d5
reebok159 <reebok159@bk.ru>
2018-10-02 12:04:59 +0300

    move src to dev branch

--- src/comments/comments.component.js
+++
@@ -1,9 +0,0 @@
-import myTemplate from './comments.html';
-
-(function() {
-  'use strict';
-
-	angular.module('todoListClient')
-	  .component('commentsTpl', { template: myTemplate })
-
-})();
--- src/projects/projects.component.js
+++
@@ -1,9 +0,0 @@
-import myTemplate from './projects.modals.html';
-
-(function() {
-  'use strict';
-
-	angular.module('todoListClient')
-	  .component('projectsModals', { template: myTemplate })
-
-})();
